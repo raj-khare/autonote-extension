9f5a9663a544ff31b8b0ba1a06795a1932a70e5d
Raj Khare <seven.triangles@icloud.com>
2020-04-04 18:09:58 +0530

    Change model to distilbert-base-uncased (smaller)

--- app.py
+++ app.py
@@ -14,16 +14,15 @@ def notes():
     url = data['url']
     if not re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", url):
         return jsonify({'error': 'Bad url format'}), 400
-
     article = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).text
     article_parsed = BeautifulSoup(article, 'html.parser')
     paragraphs = article_parsed.find_all('p')  # Getting all <p>CONTENT</p>
     article_content = []
     for p in paragraphs:
         article_content.append(p.text)
-    model = Summarizer()
+    model = Summarizer(model='distilbert-base-uncased')
     return jsonify({'notes': model("".join(article_content))})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
